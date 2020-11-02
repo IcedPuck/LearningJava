7a252493b0d9da87368b66759d4fbbc81f7ba86f
IcedPuck <645612410@qq.com>
2020-11-02 12:43:55 +0800

    2/11/2020

--- src/com/demo/java/Primary01_11_2020/ClassObejct_Packet.java
+++ src/com/demo/java/Primary01_11_2020/ClassObejct_Packet.java
@@ -1,4 +1,22 @@
 package com.demo.java.Primary01_11_2020;
+import com.demo.java.Primary01_11_2020.Inheritance.Weapon_Item;
 
 public class ClassObejct_Packet {
+    public void step1(){
+        String name; //姓名
+
+        float hp; //血量
+
+        float armor; //护甲
+
+        int moveSpeed; //移动速度
+    }
+    public void step2(Weapon_Item w){
+
+    }
+
+    public static void main(String[] args) {
+        ClassObejct_Packet packet = new ClassObejct_Packet();
+        packet.step1();
+    }
 }
